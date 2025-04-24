9b73c818a3b1de428e5d39074e117424687b28ce
Sachin Anand <ssachin520280@outlook.com>
2025-04-24 21:18:14 +0530

    bug fixed

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -28,7 +28,7 @@ contract SendPackedUserOp is Script {
         bytes32 s;
         uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         if (block.chainid == 31337) {
-            (v, r, s) = vm.sign(config.account, digest);
+            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
         } else {
             (v, r, s) = vm.sign(config.account, digest);
         }
